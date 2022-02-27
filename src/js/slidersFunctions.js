09d054e561ec7f3f58ddb5c00a698433284076fe
Aipov Shamil <xphnx@ya.ru>
2022-02-27 17:09:06 +0300

    script was refactored to manage several sliders

--- src/js/slidersFunctions.js
+++ src/js/slidersFunctions.js
@@ -1,28 +1,37 @@
 import Swiper, {Pagination} from "swiper";
 
-let brandSlider = null;
+  const sliders = [
+  {selector: '.brands__list'},
+  {selector: '.services__list'},
+  {selector: '.prices__list'}
+];
+
 const mediaQuerySize = 768;
 
-function SlidersInit () {
-  if (!brandSlider) {
-    brandSlider = new Swiper('.swiper', {
-      modules: [Pagination],
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      slidesPerView: "auto",
-      spaceBetween: 16
-    });
-  }
+function SlidersInit (sliders) {
+  sliders.forEach(slider => {
+      if (!slider.slider) {
+        slider.slider = new Swiper(slider.selector, {
+          modules: [Pagination],
+          pagination: {
+            el: '.swiper-pagination',
+          },
+          slidesPerView: "auto",
+          spaceBetween: 16
+        });
+      }
+  });
 }
 
-function SlidersDestroy () {
-  if (brandSlider) {
-    brandSlider.destroy();
-    brandSlider = null;
-  }
+function SlidersDestroy (sliders) {
+  sliders.forEach(slider => {
+    if (slider.slider) {
+      slider.slider.destroy();
+      slider.slider = null;
+    }
+  });
 }
 
 export function manageSlidesShow() {
-  window.innerWidth < mediaQuerySize ? SlidersInit() : SlidersDestroy();
+  window.innerWidth < mediaQuerySize ? SlidersInit(sliders) : SlidersDestroy(sliders);
 }
