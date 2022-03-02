b296273e15105ea88efd5228ed4c97536d768588
Aipov Shamil <xphnx@ya.ru>
2022-03-02 16:04:29 +0300

    refactoring

--- src/js/menuInMobileBrowsers.js
+++ src/js/menuInMobileBrowsers.js
@@ -4,19 +4,56 @@ export function fixViewportUnits() {
 }
 
 export function manageMenuBehavior() {
-  const openButton = document.querySelector('.rounded-button--icon--burger');
-  const closeButton = document.querySelector('.rounded-button--icon--close-menu');
+  const burger = document.querySelector('.rounded-button--icon--burger');
+  const closeButtons = document.querySelectorAll('.rounded-button--icon--close');
   const menu = document.querySelector('.menu');
+  const modalFeedback = document.querySelector('.modal__feedback')
+  const modalCallBack = document.querySelector('.modal__callback');
   const wrapper = document.querySelector('.main');
+  const buttonsCallBack = document.querySelectorAll('.rounded-button--icon--callback');
+  const buttonsMessage = document.querySelectorAll('.rounded-button--icon--message');
 
-  function toggleMenu(toggle) {
-    wrapper.classList[toggle]('menu-opened');
+  function toggleModal(toggle, modal) {
+    const element = chooseModal(modal);
+
+    wrapper.classList[toggle](`${modal}-opened`);
     document.body.classList[toggle]('overflow-hidden');
-    menu.classList[toggle]('menu--opened');
+    element.classList[toggle](`${modal}--opened`);
   }
 
-  openButton.addEventListener('click', () => toggleMenu('add'));
-  closeButton.addEventListener('click',() =>  toggleMenu('remove'));
-  document.addEventListener('keydown', e => e.key === 'Escape' ? toggleMenu('remove') : '');
-  wrapper.addEventListener('click', () => toggleMenu('remove'));
+  function chooseModal(modal) {
+    return modal === 'menu' ? menu
+      : modal === 'modal__feedback' ? modalFeedback : modalCallBack;
+  }
+
+  function manageCloseSequence() {
+    let element = 'menu';
+
+    if (wrapper.classList.contains('modal__feedback-opened')) {
+      element = 'modal__feedback';
+    } else if (wrapper.classList.contains('modal__callback-opened')) {
+      element = 'modal__callback';
+    }
+    return element;
+  }
+
+  burger.addEventListener('click', () => toggleModal('add', 'menu'));
+
+  buttonsCallBack.forEach(button => {
+    button.addEventListener('click', () => toggleModal('add', 'modal__feedback'))
+  });
+
+  buttonsMessage.forEach(button => {
+    button.addEventListener('click', () => toggleModal('add', 'modal__callback'))
+  });
+
+  closeButtons.forEach(button => {
+    button.addEventListener('click', () => toggleModal('remove', manageCloseSequence()));
+  })
+
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') toggleModal('remove', manageCloseSequence());
+  });
+
+  wrapper.addEventListener('click', () => toggleModal('remove', manageCloseSequence()));
 }
