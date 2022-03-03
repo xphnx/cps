afb7131312f1345890bb035698b8684fab3b1511
Aipov Shamil <xphnx@ya.ru>
2022-03-03 16:18:23 +0300

    minor chnages

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,10 @@ import 'swiper/modules/pagination/pagination.scss';
 import '../scss/style.scss';
 
 import {manageSlidesShow} from "./slidersFunctions";
-import {fixViewportUnits, manageMenuBehavior} from "./menuInMobileBrowsers";
+import {fixViewportUnits, manageModalBehavior} from "./modalsInMobileBrowsers";
 
 window.addEventListener('DOMContentLoaded', () => {
-  manageMenuBehavior();
+  manageModalBehavior();
   fixViewportUnits();
   manageSlidesShow();
 });
